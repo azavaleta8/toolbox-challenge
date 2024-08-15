55e4c178fd3b4f928028e1d2a20d28d2a7fe51f1
abelzavaleta <abelzavaleta08@gmail.com>
2024-08-15 15:46:43 -0400

    Backend: File service

---
+++ backend/src/services/fileService.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+
+const API_BASE_URL = 'https://echo-serv.tbxnet.com/v1/secret';
+const API_KEY = process.env.API_KEY ||'aSuperSecretKey';
+
+exports.getFilesList = async () => {
+	const response = await axios.get(`${API_BASE_URL}/files`, {
+		headers: { 
+            authorization: `Bearer ${API_KEY}`
+        },
+	});
+	return response.data.files;
+};
+
+exports.getFileContent = async (fileName) => {
+	const response = await axios.get(`${API_BASE_URL}/file/${fileName}`, {
+		headers: { 
+            authorization: `Bearer ${API_KEY}`
+        },
+	});
+    console.log(response.data)
+	return response.data;
+};
+
+exports.processCSVContent = (content) => {
+	const lines = content.split('\n');
+	const header = lines[0].split(',');
+	const processedLines = [];
+
+	for (let i = 1; i < lines.length; i += 1) {
+		const values = lines[i].split(',');
+		if (values.length === header.length) {
+			processedLines.push({
+				text: values[1],
+				number: parseInt(values[2], 10),
+				hex: values[3],
+			});
+		}
+	}
+
+	return processedLines;
+};
+
+exports.getProcessedFilesData = async (fileName) => {
+	const files = fileName ? [fileName] : await this.getFilesList();
+	const processedData = [];
+
+	await Promise.all(files.map(async (file) => {
+		try {
+			const content = await this.getFileContent(file);
+			const lines = this.processCSVContent(content);
+			if (lines.length > 0) {
+				processedData.push({ file, lines });
+			}
+		} catch (error) {
+			console.error(`Error processing file ${file}:`, error.message)
+		}
+	}));
+
+	return processedData;
+};
